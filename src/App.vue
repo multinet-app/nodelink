131d5c71a3a5468f48c2f49afa5f9e281ab062ef
Roni Choudhury <roni.choudhury@kitware.com>
2019-05-13 11:13:56 -0400

    Assemble node subset + incident edges for NodeLink component

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,39 @@
   <div id="app">
     <h1>Welcome to Your Vue.js App</h1>
     <span>{{ message }}</span>
+    <node-link :graph="graph" />
   </div>
 </template>
 
 <script>
+  import NodeLink from '@/components/NodeLink';
+
   import { graphql } from '@/util/multinet';
+
+  async function getNodes (workspace, graph, nodeType) {
+    const limit = 10;
+    const result = await graphql(`query {
+        nodes (workspace: "${workspace}", graph: "${graph}", nodeType: "${nodeType}") {
+          total
+          nodes (limit: ${limit}) {
+            key
+          }
+        }
+      }`);
+    return result.data.nodes.nodes;
+  }
 
   export default {
     name: 'app',
 
     components: {
-
+      NodeLink
     },
 
     data () {
       return {
         message: '(waiting for data)',
+        graph: null,
       };
     },
 
@@ -32,21 +49,78 @@
         return;
       }
 
-      const json = await graphql(`query {
-        nodes (workspace: "${workspace}", graph: "${graph}", nodeType: "author") {
-          total
-          nodes (limit: 100) {
-            key
+      const authors = await getNodes(workspace, graph, 'author');
+      const conf = await getNodes(workspace, graph, 'conference');
+      const journal = await getNodes(workspace, graph, 'journal');
+      const nodes = [].concat(authors, conf, journal);
+
+      const edges = await this.getEdges(workspace, 'authorship', nodes);
+
+      this.graph = {
+        nodes,
+        edges
+      };
+    },
+
+    methods: {
+      async getEdges (workspace, table, nodes) {
+        const keys = new Set();
+        nodes.forEach((node) => {
+          keys.add(node.key);
+        });
+
+        const countQ = await graphql(`query {
+          rows (workspace: "${workspace}", table: "${table}") {
+            total
           }
+        }`);
+        const count = countQ.data.rows.total;
+
+        let offset = 0;
+        const limit = 10000;
+        let finalEdges = [];
+        while (offset < count) {
+          this.message = `Processing edges ${offset} through ${Math.min(offset +
+          limit, count + 1)}`;
+
+          const edgeQ = await graphql(`query {
+            rows (workspace: "${workspace}", table: "${table}") {
+              total
+              rows (offset: ${offset}, limit: ${limit}) {
+                key
+                columns (keys: ["_from", "_to"]) {
+                  key
+                  value
+                }
+              }
+            }
+          }`);
+
+          const edges = edgeQ.data.rows.rows.map((edge) => {
+            let result = {
+              key: edge.key
+            };
+
+            edge.columns.forEach((col) => {
+              result[col.key.slice(1)] = col.value;
+            });
+
+            return result;
+          });
+
+          offset += edges.length;
+
+          edges = edges.filter((edge) => {
+            return keys.has(edge.from) || keys.has(edge.to);
+          });
+
+          finalEdges = finalEdges.concat(edges);
         }
-      }`);
 
-      if (json.errors.length > 0) {
-        this.message = json.errors.join(', ');
-        return;
-      }
+        this.message = `Processed ${count} edges; found ${finalEdges.length} edges`
 
-      this.message = `${json.data.nodes.total} nodes`;
-    }
+        return finalEdges;
+      },
+    },
   };
 </script>
---
+++ src/components/NodeLink.vue
@@ -0,0 +1,26 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg">
+    <g class="links" />
+    <g class="nodes" />
+  </svg>
+</template>
+
+<script>
+  export default {
+    name: 'node-link',
+    props: {
+      graph: Object,
+    },
+    watch: {
+      graph (newGraph) {
+        this.update();
+      },
+    },
+    methods: {
+      update () {
+        const graph = this.$props.graph;
+        console.log(graph);
+      },
+    },
+  }
+</script>
